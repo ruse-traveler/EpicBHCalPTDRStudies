00a785af37ecaf5b32d94234ca198b2b47d1cd7f
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-09-05 10:40:09 -0400

    Add steering files and some useful npsim scripts

---
+++ generation/steering/SingleParticle/electron/central.e20pos.py
@@ -0,0 +1,11 @@
+from DDSim.DD4hepSimulation import DD4hepSimulation
+from g4units import mm, GeV, MeV, degree
+
+SIM = DD4hepSimulation()
+SIM.gun.energy       = 20*GeV
+SIM.gun.particle     = "e+"
+SIM.gun.multiplicity = 1
+SIM.gun.position     = (0.0, 0.0, 0.0)
+SIM.gun.distribution = "cos(theta)"
+SIM.gun.thetaMin     = 33.5*degree
+SIM.gun.thetaMax     = 146*degree
---
+++ generation/steering/SingleParticle/gamma/central.e2gam.py
@@ -0,0 +1,11 @@
+from DDSim.DD4hepSimulation import DD4hepSimulation
+from g4units import mm, GeV, MeV, degree
+
+SIM = DD4hepSimulation()
+SIM.gun.energy       = 2*GeV
+SIM.gun.particle     = "gamma"
+SIM.gun.multiplicity = 1
+SIM.gun.position     = (0.0, 0.0, 0.0)
+SIM.gun.distribution = "cos(theta)"
+SIM.gun.thetaMin     = 33.5*degree
+SIM.gun.thetaMax     = 146*degree
