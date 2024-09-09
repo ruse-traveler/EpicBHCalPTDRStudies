8ebb213e0a9f75d09b93eebd9247c33ee75be1d4
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-09-09 18:48:54 -0400

    Add NTupleHelper to utilities

---
+++ utility/NTupleHelper.hxx
@@ -0,0 +1,102 @@
+/// ===========================================================================
+/*! \file   NTupleHelper.hxx
+ *  \author Derek Anderson
+ *  \date   09.09.2024
+ *
+ *  A lightweight class to help work with TNTuple's.
+ */
+/// ===========================================================================
+
+#ifndef NTupleHelper_hxx
+#define NTupleHelper_hxx
+
+// c++ utilities
+#include <map>
+#include <string>
+#include <vector>
+#include <cassert>
+// root libraries
+#include <TNtuple.h>
+
+
+
+// ============================================================================
+//! NTuple Helper
+// ===========================================================================
+/*! A small class to help work with
+ *  ROOT TNtuple's.
+ */
+class NTupleHelper {
+
+  private:
+
+    // data members
+    std::vector<float>           m_values;
+    std::vector<std::string>     m_variables;
+    std::map<std::string, float> m_index;
+
+  public:
+
+    // ------------------------------------------------------------------------
+    //! Getters
+    // ------------------------------------------------------------------------
+    inline std::vector<float>       GetValues()    const {return m_values;}
+    inline std::vector<std::string> GetVariables() const {return m_variables;}
+
+    // ------------------------------------------------------------------------
+    //! Set a variable
+    // ------------------------------------------------------------------------
+    inline void SetVariable(const std::string& var, const float val) {
+
+      // check if variable exists
+      if (!m_index.count(var)) {
+        assert(m_index.count(var));
+      }
+
+      // then set variable
+      m_values.at(m_index[var]) = val;
+      return;
+
+    }  // end 'SetVariable(std::string&, float)'
+
+    // ------------------------------------------------------------------------
+    //! Compress list of variables into a colon-seperated list
+    // ------------------------------------------------------------------------
+    inline std::string CompressVariables() const {
+
+      std::string compressed("");
+      for (std::size_t iVar = 0; iVar < m_variables.size(); ++iVar) {
+        compressed.append(m_variables[iVar]);
+        if (iVar + 1 < m_variables.size()) {
+          compressed.append(":");
+        }
+      }
+      return compressed;
+
+    }  // end 'CompressVariables()'
+
+    // ------------------------------------------------------------------------
+    //! Default ctor/dtor
+    // ------------------------------------------------------------------------
+    NTupleHelper()  {};
+    ~NTupleHelper() {};
+
+    // ------------------------------------------------------------------------
+    //! ctor accepting a list of variables
+    // ------------------------------------------------------------------------
+    NTupleHelper(const std::vector<std::string>& vars) {
+
+      m_variables = vars;
+      for (std::size_t iVar = 0; const std::string& var : m_variables) {
+        m_index[var] = iVar;
+        ++iVar;
+      }
+      m_values.resize(m_variables.size());
+
+    }  // end ctor(std::vector<std::string>&)'
+
+};  // end NTupleHelper
+
+#endif
+
+// end ========================================================================
