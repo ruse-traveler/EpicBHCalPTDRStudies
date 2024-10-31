f6492973d429c3470c36c20be743b8c2573b0072
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-10-31 12:28:30 -0400

    Add skeleton of plotter class

---
+++ plotting/plotter/BHCalPlotter.hxx
@@ -0,0 +1,42 @@
+/// ===========================================================================
+/*! \file   BHCalPlotter.hxx
+ *  \author Derek Anderson
+ *  \date   10.31.2024
+ *
+ *  A class to consolidate plotting routines for
+ *  the BHCal chapter of the ePIC pTDR.
+ */
+/// ===========================================================================
+
+// c++ utilities
+/* TODO will go here */
+// root libraries
+/* TODO will go here */
+// analysis utilities
+#include "../../utility/PlotHelper.hxx"
+
+
+
+// ============================================================================
+//! BHCal pTDR plotting routines
+// ============================================================================
+/*! This class conslidates plotting routines
+ *  for the BHCal chapter of the ePIC pTDR.
+ */
+class BHCalPlotter {
+
+  public:
+
+    // default ctor/dtor
+    BHCalPlotter()  {};
+    ~BHCalPlotter() {};
+
+    /* TODO fill in */
+
+  private:
+
+    /* TODO fill in */
+
+};  // end BHCalPlotter
+
+// end ========================================================================
