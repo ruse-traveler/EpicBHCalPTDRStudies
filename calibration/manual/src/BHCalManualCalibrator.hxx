9a36ae3d3709bf8dd91315ce23a51efea27a25bd
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-11-20 19:53:15 -0500

    Begin filling in

--- calibration/manual/src/BHCalManualCalibrator.hxx
+++ calibration/manual/src/BHCalManualCalibrator.hxx
@@ -20,15 +20,47 @@
 #include <iostream>
 // root libraries
 #include <TF1.h>
+#include <TH1.h>
 #include <TFile.h>
+#include <TGraph.h>
 #include <TNtuple.h>
-#include <TGraphErrors.h>
+#include <TGraph2D.h>
+// dataframe related classes
+#include <ROOT/RDataFrame.hxx>
+#include <ROOT/RDF/HistoModels.hxx>
 // analysis utilities
 #include "../../utils/HistHelper.hxx"
+#include "../../utils/GraphHelper.hxx"
 #include "../../utils/NTupleHelper.hxx"
 
 // abbreviate common namespaces
 namespace HH = HistHelper;
+namespace GH = GraphHelper;
+
+
+
+// ============================================================================
+//! User options for manual calibration
+// ============================================================================
+struct BHCalManualCalibratorConfig {
+
+  // input parameters
+  std::string infile  = "test.root";
+  std::string intuple = "ntForCalib";
+
+  // tuple parameters
+  std::string eParLeaf = "ePar";
+  std::string eEMLeaf  = "eSumBEMC";
+  std::String eHadLeaf = "eSumBHCal";
+
+  // cut parameters
+  std::vector<double>       ePar     = {1.};
+  std::pair<double, double> eParBins = {0., 100.};
+
+  // mu/sigma parameters
+  std::vector<double> relValues = {1.0};
+
+};  // end BHCalManualCalibratorConfig
 
 
 
@@ -43,15 +75,42 @@ class BHCalManualCalibrator {
 
   public:
 
+    // enumeration of methods
+    enum Method {Chi2, MuSigma};
+
+
     // default ctor/dtor
     BHCalManualCalibrator();
     ~BHCalManualCalibrator();
 
-    /* TODO fill in */
+    // ctor accepting a configuration
+    BHCalManualCalibrator(const BHCalManualCalibratorConfig& cfg);
+
+    // public methods
+    void Init();
+    void DoChiSquareMinimization();
+    void DoMuSigmaMinimization();
+    void End(TFile* outfile);
 
   private:
 
-    /* TODO fill in */
+    // private methods
+    double      GetSigmaOverMu(const double rel, const std::size_t iHist, const std::size_t iParBin);
+    std::size_t GetHistIndex();  // TODO
+    std::string GetParCutString(const std::size_t iParBin); 
+
+    // hist/graph definitions
+    std::vector<HH::Definition> m_histDefs;
+    std::vector<GH::Definition> m_graphDefs;
+
+    // outputs
+    std::vector<TH1*>      m_hists;
+    std::vector<TF1*>      m_funcs;
+    std::vector<TGraph*>   m_graphs;
+    std::vector<TGraph2D*> m_graph2Ds;
+
+    // configuration
+    BHCalManualCalibratorConfig m_cfg;
 
 };  // end BHCalManualCalibrator
 
