a608353729d67ddd7af62cd02b1f9e10f71b2b14
ruse-traveler <derek.murphy.anderson@protonmail.com>
2025-03-11 15:46:35 -0400

    Make EICrecon script easier

--- reconstruction/scripts/RunEICRecon.rb
+++ reconstruction/scripts/RunEICRecon.rb
@@ -8,22 +8,16 @@
 # =============================================================================
 
 # i/o parameters
-in_ddsim   = "../forTestingJetPodioRelations.edm4hep.root"
-out_podio  = "forTestingUserConfigurability_change1V2_withMinJetPtAndMinCstPtChanged_recoJetAlgoSetToGarbage_reco2gen12.podio.root"
-out_plugin = "forTestingUserConfigurability_change1V2_withMinJetPtAndMinCstPtChanged_recoJetAlgoSetToGarbage_reco2gen12.plugin.root"
+in_ddsim   = "./test.npsim_output.edm4hep.root"
+out_podio  = "test.eicrecon_output.edm4eic.root"
+out_plugin = "test.eicrecon_output.plugin.root"
 
 # output collections from EICrecon
 out_collect = [
-  "GeneratedParticles",
-  "GeneratedJets",
-  "GeneratedChargedJets",
-  "ReconstructedJets",
-  "ReconstructedChargedJets"
 ].compact.reject(&:empty?).join(',')
 
 # plugins to run in EICrecon
 plugins = [
-  "dump_flags"
 ].compact.reject(&:empty?).join(',')
 
 # options
@@ -32,6 +26,6 @@ options = [
 ].compact.reject(&:empty?).join(' ')
 
 # run EICrecon
-exec("eicrecon -Pplugins=#{plugins} -Ppodio:output_collections=#{out_collect} #{options} -Ppodio:output_file=#{out_podio} -Phistsfile=#{out_plugin} #{in_ddsim}")
+exec("eicrecon -Pplugins=#{plugins} -Ppodio:output_collections=#{out_collect} #{options} -Ppodio:output_file=#{out_podio} #{in_ddsim}")
 
 # end =========================================================================
